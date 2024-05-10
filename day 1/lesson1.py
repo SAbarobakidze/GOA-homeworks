734552fedd2ce38d57afbbb70fa157e24ffe9103
SAbarobakidze <robakidze2009saba@gmail.com>
2024-05-10 04:33:11 -0700

    second commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,7 @@
+name = "saba"
+surname = "robakidze"
+age = 15
+height = 189,5
+knows_basketball = True
+knows_soccer = False 
+print(name + " "+ surname + " "+ str(age) + " "+ str(height) + " "+ str(knows_basketball) + " "+ str(knows_soccer))
