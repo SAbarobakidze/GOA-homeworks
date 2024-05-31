0ee3fe355d6e4cabcafc68bcce3afe69d63d4b69
SAbarobakidze <robakidze2009saba@gmail.com>
2024-05-31 04:36:54 -0700

    second commit

---
+++ level3/homework.py
@@ -0,0 +1,19 @@
+#1
+Name = 'saba'
+last_name = 'Robakidze'
+age = 15
+hobby ='basketball_player'
+country = 'Georgia'
+knows_how_to_cook_food = True
+Closet1 = "clothes,toyes."
+Closet2 = "shoes,shoe_boxes,yoga mat"
+num1 = 65
+num2 = 54
+num3 = 5.5
+num4 = 9.9
+
+print(Name +" " +  last_name +" "+ str(age) +" "+ hobby +" "+ country)
+print (num1 + num2 * num1 / num3)
+print(num3 + num1 * num4 / num3 + num2)
+print(Closet1 + " " + Closet2)
+
